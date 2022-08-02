14bed40f366fb60b8eb55834fe17882277faa085
Rusadi <rusadi@detik.com>
2022-08-02 09:08:17 +0700

    Initial commit

--- helper.py
+++ helper.py
@@ -1,110 +1,376 @@
 import pandas
 
-def WriteToExcel(data, sheetname):
+def WriteToExcel(destination, data, sheetname, index):
     with pandas.ExcelWriter(
-        "/Users/rusadi/Project/excel-python/omzet.xlsx",
+        destination,
         mode="a",
         engine="openpyxl",
         if_sheet_exists="replace",
     ) as writer:
-        data.to_excel(writer, sheet_name=sheetname, index=False) 
+        data.to_excel(writer, sheet_name=sheetname, index=index) 
 
 def CleanServiceName(argument):
-    service_name = argument.title()
+    unused_name  = {'(new)', '(N)', '(baru)', '(baru )', '( baru )', 'New', '( baru)'}
+    checked      = any(element in argument for element in unused_name)
+    service_name = ' '.join(argument.split(' ')[:-1]) if checked else argument.title()
+    category     = ''
 
-    if 'blow' in argument.lower():
-        service_name = 'Blow'
-    if 'cut woman' in argument.lower():
-        service_name = 'Haircut Woman'
-    if 'cut man' in argument.lower():
-        service_name = 'Haircut Man'
-    if 'poni' in argument.lower():
-        service_name = 'Haircut Bangs'
-    if 'inoa' in argument.lower():
-        service_name = 'Coloring Inoa'
-    if 'fashion color' in argument.lower():
-        service_name = 'Fashion Color'
-    if 'color' in argument.lower():
-        service_name = 'Coloring'
-    if 'oleoshape' in argument.lower():
-        service_name = 'Oleoshape'
-    if 'keratin' in argument.lower():
-        service_name = 'Coco Keratin'
-    if 'dry' in argument.lower():
-        service_name = 'Dry Only'
-    if 'repair full' in argument.lower():
-        service_name = 'Dry Only'
-    if 'sanggul' in argument.lower():
-        service_name = 'Upstyle'
-    if 'upstyle' in argument.lower():
-        service_name = 'Upstyle'
-    if 'smoothing' in argument.lower():
-        service_name = 'Smoothing'
-    if 'higlight' in argument.lower():
-        service_name = 'Highlight'
-    if 'highlight' in argument.lower():
-        service_name = 'Highlight'
-    if 'extra loreal 1' in argument.lower():
-        service_name = 'Extra Loreal 1'
-    if 'extra loreal 1/2' in argument.lower():
-        service_name = 'Extra Loreal 1/2'
-    if 'shampo' in argument.lower():
-        service_name = 'Shampoo'
-    if 'shampo naturica' in argument.lower():
-        service_name = 'Shampoo Naturica'
-    if 'netral' in argument.lower():
-        service_name = 'Netral'
-    if 'back massage' in argument.lower():
+    if 'back massage' in service_name.lower():
         service_name = 'Back Massage'
-    if 'hairlos' in argument.lower():
-        service_name = 'Treatment Hairloss'
-    if 'cr hair & scalp complete' in argument.lower():
+        category = 'massage'
+    elif 'blow' in service_name.lower():
+        service_name = 'Blow'
+        category = 'blow'
+    elif 'blow extention' in service_name.lower():
+        service_name = 'Blow Extention'
+        category = 'blow'
+    elif 'coco keratin l' in service_name.lower():
+        service_name = 'Coco Keratin Long'
+        category = 'chemical'
+    elif 'color l ' in service_name.lower():
+        service_name = 'Coloring Long '
+        category = 'chemical'
+    elif 'coloring l' in service_name.lower():
+        service_name = 'Coloring Long'
+        category = 'chemical'
+    elif 'coloring s' in service_name.lower():
+        service_name = 'Coloring Short'
+        category = 'chemical'
+    elif 'cr anti hairloss' in service_name.lower():
+        service_name = 'Treatment Anti Hairloss'
+        category = 'treatment'
+    elif 'crimbath complite' in service_name.lower():
         service_name = 'Treatment Complete'
-    if 'dd cream pemakian' in argument.lower():
-        service_name = 'Cream DD Pemakaian'
-    if 'hair tonic' in argument.lower():
+        category = 'treatment'
+    elif 'dd cream pemakian' in service_name.lower():
+        service_name = 'DD Cream Pemakaian'
+        category = 'product'
+    elif 'dry only' in service_name.lower():
+        service_name = 'Dry Only'
+        category = 'blow'
+    elif 'energing pemakian' in service_name.lower():
+        service_name = 'Energing Pemakaian'
+        category = 'product'
+    elif 'Fashion Color' in service_name.lower():
+        service_name = 'Fashion Color'
+        category = 'chemical'
+    elif 'hair cut women' in service_name.lower():
+        service_name = 'Haircut Women'
+        category = 'haircut'
+    elif 'hair spa' in service_name.lower():
+        service_name = 'Hairspa'
+        category = 'treatment'
+    elif 'hair tonic' in service_name.lower():
         service_name = 'Hair Tonic'
-    if 'hairmask repair expert ' in argument.lower():
-        service_name = 'Hairmask Repair Expert'
-    if 'hair spa' in argument.lower():
-        service_name = 'Loreal Hairspa'
-    if 'hairspa' in argument.lower():
-        service_name = 'Loreal Hairspa'
-    if 'hair spa' in argument.lower():
-        service_name = 'Loreal Hairspa'
-    if 'naturica' in argument.lower():
-        service_name = 'Treatment Naturica'
-    if 'cromearth' in argument.lower():
-        service_name = 'Naturica Mask Cromearth'
-    if 'repairing deep shm 250ml ' in argument.lower():
-        service_name = 'Repairing Deep Shampoo 250 ml'
-    if 'naturica repairing shm 250ml' in argument.lower():
-        service_name = 'Naturica Repairing Shampoo 250 ml'
-    if 'repairing shm 250ml' in argument.lower():
-        service_name = 'Naturica Repairing Shampoo 250 ml'
-    if 'repairing deep shampo 1000ml' in argument.lower():
-        service_name = 'Repairing Deep Shampo 1000 ml'
-    if 'foot polish' in argument.lower():
+        category = 'product'
+    elif 'moisturazing shampo 250 ml' in service_name.lower():
+        service_name = 'Moisturazing Shampoo 250ml'
+        category = 'product'
+    elif 'naturica energizing treatment 100ml' in service_name.lower():
+        service_name = 'Naturica Energizing Treatment 100ml'
+        category = 'product'
+    elif 'naturica treatment' in service_name.lower():
+        service_name = 'Naturica Treatment'
+        category = 'treatment'
+    elif 'naturica' in service_name.lower():
+        service_name = 'Naturica Treatment'
+        category = 'treatment'
+    elif 'netral' in service_name.lower():
+        service_name = 'Netral'
+        category = 'shampoo'
+    elif 'opuntia oil pemakaian' in service_name.lower():
+        service_name = 'Opuntia Oil Pemakaian'
+        category = 'product'
+    elif 'reflexi' in service_name.lower():
+        service_name = 'Foot Reflexy'
+        category = 'massage'
+    elif 'repair full' in service_name.lower():
+        service_name = 'Repair Full'
+        category = 'extention'
+    elif 'shampoo' in service_name.lower():
+        service_name = 'Shampoo'
+        category = 'shampoo'
+    elif 'treatment anti hairlos' in service_name.lower():
+        service_name = 'Treatment Anti Hairloss'
+        category = 'treatment'
+    elif 'vitamin' in service_name.lower():
+        service_name = 'Vitamin'
+        category = 'product'
+    elif 'anti dendruf' in service_name.lower():
+        service_name = 'Treatment Anti Dandruf'
+        category = 'treatment'
+    elif 'cr hair & scalp' in service_name.lower():
+        service_name = 'Treatment Complete'
+        category = 'treatment'
+    elif 'crimbath complite' in service_name.lower():
+        service_name = 'Treatment Complete'
+        category = 'treatment'
+    elif 'foot polish opi' in service_name.lower():
         service_name = 'Foot Polish OPI'
-    if 'half leg rica wax' in argument.lower():
+        category = 'polish'
+    elif 'hair spa' in service_name.lower():
+        service_name = 'Hairspa'
+        category = 'treatment'
+    elif 'half leg rica wax' in service_name.lower():
         service_name = 'Half Leg Rica Wax'
-    if 'hand polish' in argument.lower():
-        service_name = 'Hand Polish OPI'
-    if 'manicure' in argument.lower():
+        category = 'polish'
+    elif 'hand polis opi' in service_name.lower():
+        service_name = 'Hand Polis OPI'
+        category = 'polish'
+    elif 'manicure' in service_name.lower():
         service_name = 'Manicure Gehwol'
-    if 'pedicure' in argument.lower():
+        category = 'mp'
+    elif 'naturica detox' in service_name.lower():
+        service_name = 'Naturica Detox'
+        category = 'treatment'
+    elif 'naturica energizing tratement 100ml' in service_name.lower():
+        service_name = 'Naturica Energizing 100ml'
+        category = 'product'
+    elif 'netral' in service_name.lower():
+        service_name = 'Netral'
+        category = 'shampoo'
+    elif 'pedicure' in service_name.lower():
         service_name = 'Pedicure Gehwol'
-    if 'scrub foot' in argument.lower():
-        service_name = 'Foot Scrub'
-    if 'scrub foot' in argument.lower():
-        service_name = 'Hand Scrub'
-    if 'dandruf' in argument.lower():
-        service_name = 'Treatment Dandruff'
-    if 'foot polish' in argument.lower():
-        service_name = 'Foot Polish'
-    if 'reflexy' in argument.lower():
+        category = 'mp'
+    elif 'reflexy' in service_name.lower():
         service_name = 'Foot Reflexy'
-    if 'reflexi' in argument.lower():
-        service_name = 'Foot Reflexy'
+        category = 'massage'
+    elif 'soothing shampo 1000 ml' in service_name.lower():
+        service_name = 'Shoothing Shampo 1000ml'
+        category = 'product'
+    elif 'foot gell opie' in service_name.lower():
+        service_name = 'Foot Gell OPI'
+        category = 'polish'
+    elif 'hair tonic' in service_name.lower():
+        service_name = 'Hair Tonic'
+        category = 'product'
+    elif 'hairspa' in service_name.lower():
+        service_name = 'Hairspa'
+        category = 'treatment'
+    elif 'blow' in service_name.lower():
+        service_name = 'Blow'
+        category = 'blow'
+    elif 'coloring inoa s' in service_name.lower():
+        service_name = 'Coloring Inoa Short'
+        category = 'chemical'
+    elif 'coloring l 1' in service_name.lower():
+        service_name = 'Coloring Long'
+        category = 'chemical'
+    elif 'cut man' in service_name.lower():
+        service_name = 'Haircut Man'
+        category = 'haircut'
+    elif 'cut women' in service_name.lower():
+        service_name = 'Haircut Woman'
+        category = 'haircut'
+    elif 'extra loreal 1' in service_name.lower():
+        service_name = 'Extra Loreal 1'
+        category = 'product'
+    elif 'extra loreal 1/2' in service_name.lower():
+        service_name = 'Extra Loreal 1/2'
+        category = 'product'
+    elif 'fashion color' in service_name.lower():
+        service_name = 'Fashion Color'
+        category = 'chemical'
+    elif 'hair coloring long' in service_name.lower():
+        service_name = 'Coloring Long'
+        category = 'chemical'
+    elif 'haircoloring inoa l' in service_name.lower():
+        service_name = 'Coloring Inoa Long'
+        category = 'chemical'
+    elif 'haircut man' in service_name.lower():
+        service_name = 'Haircut Man'
+        category = 'haircut'
+    elif 'haircut woman' in service_name.lower():
+        service_name = 'Haircut Woman'
+        category = 'haircut'
+    elif 'upstyle' in service_name.lower():
+        service_name = 'Upstyle/Sanggul'
+        category = 'upstyle'
+    elif '(naturica) opuntia oil shampo 250ml' in service_name.lower():
+        service_name = 'Opuntia Oil Shampoo 250ml'
+        category = 'product'
+    elif 'hairmask repair expert ' in service_name.lower():
+        service_name = 'Hairmask Repair Expert '
+        category = 'treatment'
+    elif 'shampo' in service_name.lower():
+        service_name = 'Shampoo'
+        category = 'shampoo'
+    elif 'smoothing l' in service_name.lower():
+        service_name = 'Smoothing Long'
+        category = 'chemical'
+    elif 'dry' in service_name.lower():
+        service_name = 'Dry Only'
+        category = 'blow'
+    elif 'foot polish opie' in service_name.lower():
+        service_name = 'Foot Polish OPI'
+        category = 'polish'
+    elif 'hand polish opi' in service_name.lower():
+        service_name = 'Hand Polish OPI'
+        category = 'polish'
+    elif 'waxing 1/4' in service_name.lower():
+        service_name = 'Waxing 1/4'
+        category = 'polish'
+    elif 'apuntia oil shape matt puty' in service_name.lower():
+        service_name = 'Opuntia Oil Shape Matt Putty'
+        category = 'product'
+    elif 'coco keratin s' in service_name.lower():
+        service_name = 'Coco Keratin Short'
+        category = 'chemical'
+    elif 'color inoa s' in service_name.lower():
+        service_name = 'Coloring Inoa Short'
+        category = 'chemical'
+    elif 'color l' in service_name.lower():
+        service_name = 'Coloring Long'
+        category = 'chemical'
+    elif 'coloring s 1' in service_name.lower():
+        service_name = 'Coloring Short'
+        category = 'chemical'
+    elif 'cut man' in service_name.lower():
+        service_name = 'Haircut Man'
+        category = 'haircut'
+    elif 'energizing miracle tratment sppry 100ml' in service_name.lower():
+        service_name = 'Energizing Miracle Spray 100ml'
+        category = 'product'
+    elif 'extra loreal 1' in service_name.lower():
+        service_name = 'Extra Loreal 1'
+        category = 'product'
+    elif 'hair coloring short' in service_name.lower():
+        service_name = 'Coloring Short'
+        category = 'chemical'
+    elif 'hair cut man' in service_name.lower():
+        service_name = 'Haircut Man'
+        category = 'haircut'
+    elif 'hair cut women' in service_name.lower():
+        service_name = 'hair cut women by tatang'
+        category = 'haircut'
+    elif 'hair extention' in service_name.lower():
+        service_name = 'Hair Extention'
+        category = 'extention'
+    elif 'haircut women' in service_name.lower():
+        service_name = 'Haircut Women'
+        category = 'haircut'
+    elif 'higlight' in service_name.lower():
+        service_name = 'Highlight'
+        category = 'chemical'
+    elif 'repair 1/4' in service_name.lower():
+        service_name = 'Repair 1/4'
+        category = 'extention'
+    elif 'shape matt' in service_name.lower():
+        service_name = 'Shape Matt Putty for Men'
+        category = 'product'
+    elif 'smoothing' in service_name.lower():
+        service_name = 'smoothing'
+        category = 'chemical'
+    elif 'smothing coco' in service_name.lower():
+        service_name = 'Smoothing Coco'
+        category = 'chemical'
 
-    return service_name
+    print(service_name)
+    return {
+        'service_name':  service_name,
+        'category': category
+    }
+
+def Commission():
+    return {
+        'stylist':
+        {
+            'blow': {
+                'reduction': 30,
+                'commission': 50
+            },
+            'chemical': {
+                'reduction': 50,
+                'commission': 50
+            },
+            'haircut': {
+                'reduction': 30,
+                'commission': 50
+            },
+            'product': {
+                'reduction': 0,
+                'commission': 10
+            },
+            'extention': {
+                'reduction': 0,
+                'commission': 15
+            },
+            'makeup': {
+                'reduction': 0,
+                'commission': 50
+            },
+            'upstyle': {
+                'reduction': 0,
+                'commission': 40
+            },
+            'shampoo': {
+                'reduction': 0,
+                'commission': 50
+            }
+        },
+        'therapies': {
+            
+            'blow': {
+                'reduction': 0,
+                'commission': 25
+            },
+            'chemical': {
+                'reduction': 0,
+                'commission': 15
+            },
+            'haircut': {
+                'reduction': 0,
+                'commission': 20
+            },
+            'product': {
+                'reduction': 0,
+                'commission': 10
+            },
+            'extention': {
+                'reduction': 0,
+                'commission': 15
+            },
+            'makeup': {
+                'reduction': 0,
+                'commission': 50
+            },
+            'upstyle': {
+                'reduction': 0,
+                'commission': 40
+            },
+            'treatment': {
+                'reduction': 0,
+                'commission': 15
+            },
+            'mp': {
+                'reduction': 0,
+                'commission': 20
+            },
+            'massage': {
+                'reduction': 0,
+                'commission': 40
+            },
+            'polish': {
+                'reduction': 0,
+                'commission': 15
+            },
+            'shampoo': {
+                'reduction': 0,
+                'commission': 50
+            }
+        }
+    }
+
+def Employee():
+    return {
+        'adit': 'therapies',
+        'apriyanti': 'therapies',
+        'febriana': 'therapies',
+        'marni': 'therapies',
+        'narko': 'stylist',
+        'nuy ': 'therapies',
+        'saini': 'therapies',
+        'tatang': 'stylist',
+        'sinta': 'kasir',
+        'dilla': 'kasir'
+    }
--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import pandas
 import numpy as np
+from helper import WriteToExcel, CleanServiceName, Employee, Commission
 
 # Read file excel and collect data with sheetnama rptOmzet, get col from A-H except D and skip 2 frist row
 object_data = pandas.read_excel('omzet.xlsx', sheet_name='rptOmzet', usecols='A:B, E:H', skiprows=2)
@@ -9,10 +10,57 @@ index_names = object_data.Employee.dropna().index
 last_row = object_data.Employee.index[-1]
 
 # Loop index names to get data omzet per employee
-for index in range(len(index_names)):
-    last_idx = index_names[-1]
-    target_idx = index_names[index]
+def Calculation(position, omzet, reduction, commission, idx_item, total, price, disc_value):
+    if position == 'Stylist':
+        if int(disc_value) > 10:
+            calculateCommission(omzet, reduction, commission, idx_item, price) 
+        else:
+            calculateCommission(omzet, reduction, commission, idx_item, total) 
+    else:
+        if int(disc_value) > 10:
+            calCommission(omzet, commission, idx_item, price) 
+        else:
+            calCommission(omzet, commission, idx_item, total)
 
+def calCommission(omzet, commission, idx_item, nominal):
+    omzet.loc[idx_item, 'Nett'] = (nominal * commission) / 100
+    omzet.loc[idx_item, '%'] = (omzet.loc[idx_item, 'Nett'] / nominal) * 100
+
+def calculateCommission(omzet, reduction, commission, idx_item, nominal):
+    potongan = omzet.loc[idx_item, 'Potongan'] = (nominal * reduction) / 100
+    bruto = omzet.loc[idx_item, 'Bruto'] = nominal - potongan
+    nett = omzet.loc[idx_item, 'Nett'] = (bruto * commission) / 100
+    omzet.loc[idx_item, 'potongan %'] = (potongan / nominal) * 100 
+    omzet.loc[idx_item, 'komisi %'] = (nett / bruto) * 100
+
+def generateCommission(position, omzet, index_product, last_row_product, idx, last, target, service_name, category, reduction, commission):
+    if target == last:
+        omzet.loc[target:last_row_product, 'Description'] = service_name
+        omzet.loc[target:last_row_product, 'Category'] = category
+        array_omzet = omzet.loc[target:last_row_product].index
+        CommissionPerItem(position, omzet, reduction, commission, array_omzet)
+    else:
+        next = index_product[idx + 1] - 1
+        omzet.loc[target:next, 'Description'] = service_name
+        omzet.loc[target:next, 'Category'] = category
+        array_omzet = omzet.loc[target:next].index
+        CommissionPerItem(position, omzet, reduction, commission, array_omzet)
+
+def CommissionPerItem(position, omzet, reduction, commission, array_omzet):
+    for idx_item in array_omzet:
+        total = omzet.loc[idx_item, 'Total Nett']
+        price = omzet.loc[idx_item,'Price']
+        discount = omzet.loc[idx_item,'Total Disc.']
+        disc_value = (discount / price) * 100
+                
+        print(f'Total = {total}')
+        print(f'Price = {price}')
+        print(f'Discount = {discount}')
+        print(f'Discount % = {disc_value}')
+                
+        Calculation(position, omzet, reduction, commission, idx_item, total, price, disc_value)
+
+def getDataOmzet(object_data, index_names, last_row, index, last_idx, target_idx):
     if target_idx == last_idx:
         # get data omzet each employee in last index of employee from range row and range column
         omzet_employees = object_data.iloc[target_idx:last_row, 0:7]
@@ -20,33 +68,71 @@ for index in range(len(index_names)):
         # get data omzet each employe form range row and range column
         next_index = index_names[index + 1]
         omzet_employees = object_data.iloc[target_idx:next_index, 0:7]
+    return omzet_employees
+
+
+def regenerateDataOmzet(object_data, index_names, last_row):
+    for index in range(len(index_names)):
+        last_idx = index_names[-1]
+        target_idx = index_names[index]
+
+        omzet_employees = getDataOmzet(object_data, index_names, last_row, index, last_idx, target_idx)
     
-    # Initiate employee name conver to title case 
-    sheet_name = (omzet_employees['Employee'].values[0]).title()
-    omzet = omzet_employees.iloc[0:omzet_employees.shape[0], 1:omzet_employees.shape[1]]
-    # get index service name except NaN value from column Descriptions
-    index_product = omzet.Description.dropna().index
-    # initiate last index from loop service name
-    last_row_product = omzet.Description.index[-1]
+        # Initiate employee name convert to title case 
+        sheet_name = (omzet_employees['Employee'].values[0]).title()
+        position = Employee()[sheet_name.lower()].title()
+    
+        print(sheet_name)
+        print(position)
+        print('-------------')
+    
+        omzet = omzet_employees.iloc[0:omzet_employees.shape[0], 1:omzet_employees.shape[1]]
+        # get index service name except NaN value from column Descriptions
+        index_product = omzet.Description.dropna().index
+        # initiate last index from loop service name
+        last_row_product = omzet.Description.index[-1]
+        
+        # Loop index service name
+        for idx in range(len(index_product)):
+            last = index_product[-1]
+            target = index_product[idx]
+            # Clean name of services
+            service_name = CleanServiceName(omzet.Description[target])['service_name']
+            category = CleanServiceName(omzet.Description[target])['category']
+            reduction = Commission()[position.lower()][category]['reduction']
+            commission = Commission()[position.lower()][category]['commission']
+            
+            if sheet_name == 'Saini' and category == 'mp':
+                commission = int(50)
+        
+            # Rename NaN value with service name
+            generateCommission(position, omzet, index_product, last_row_product, idx, last, target, service_name, category, reduction, commission)
+            print('---------------------')
+            
+        #  Remove parent service name
+        omzet.drop(index_product, axis=0, inplace=True)
+        # Sort data ascending by column description
+        omzet.sort_values(by=['Description'], ascending=True, inplace=True)
 
-    # Loop index service name
-    for idx in range(len(index_product)):
-        last = index_product[-1]
-        target = index_product[idx]
-        # Clean name of services
-        service_name = CleanServiceName(omzet.Description[target])
+        # write to excel with each sheetname by employee name
+        destination =  "/Users/detik/Project/extra/excel-pandas/omzet.xlsx"
+        WriteToExcel(destination, omzet, sheet_name, False)
+        
+# regenerateDataOmzet(object_data, index_names, last_row)
 
-        # Rename NaN value with service name
-        if target == last:
-            omzet.loc[target:last_row_product, 'Description'] = service_name
-        else:
-            next = index_product[idx + 1] - 1
-            omzet.loc[target:next, 'Description'] = service_name
+df_omzet = pandas.read_excel('omzet.xlsx', sheet_name=None)
+actual_sheets = [sheet for sheet in df_omzet if sheet != 'rptOmzet']
 
-    #  Remove parent service name
-    omzet.drop(index_product, axis=0, inplace=True)
-    # Sort data ascending by column description
-    omzet.sort_values(by=['Description'], ascending=True, inplace=True)
+for item in actual_sheets:
+    data_sheet = pandas.read_excel('omzet.xlsx', sheet_name=item)
+    grouped = data_sheet.groupby('Description').sum()
+    grouped.loc['Grand Total'] = grouped.sum()
+    # grouped.loc[-1] = item  # adding a row
+    # grouped.index = grouped.index + 1
+    destination = '/Users/detik/Project/extra/excel-pandas/Final Omzet.xlsx'
+    
+    WriteToExcel(destination, grouped, item, True)
+    
+    print(item)
+    print('-----------------------')
 
-    # write to excel with each sheetname by employee name
-    WriteToExcel(omzet, sheet_name)
